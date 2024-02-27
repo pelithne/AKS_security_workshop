4f0c6c9770efb28fb34b5871c223248c82b0e4c1
Peter <45140408+pelithne@users.noreply.github.com>
2024-02-27 15:56:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Update the package index and install the prerequisites for Azure CLI
-sudo apt-get update && sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo apt-get update && apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
 # Download and install the Microsoft signing key
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 # Add the Azure CLI repository to your sources list
-AZ_REPO=$(lsb_release -cs) && echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+AZ_REPO=$(lsb_release -cs) && echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
 # Install Azure CLI
-sudo apt-get update && sudo apt-get install azure-cli
+sudo apt update && 
+sudo apt install azure-cli
 # Install Docker
-sudo apt-get update && sudo apt-get install docker.io
+sudo apt install docker.io -y
 # Install AKS CLI
 az aks install-cli
-
